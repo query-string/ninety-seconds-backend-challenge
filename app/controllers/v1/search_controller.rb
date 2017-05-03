15befb4aa28ad5b4ce4913b61525032afc9ad4a2
Alex Timofeev <atimofeev@reactant.ru>
2017-05-03 13:03:11 +0700

    Add `dotenv-rails` gem

--- app/controllers/v1/search_controller.rb
+++ app/controllers/v1/search_controller.rb
@@ -1,5 +1,5 @@
 class V1::SearchController < ApplicationController
-  URL_ORIGIN = "https://api.spotify.com/v1/search?type=artist&limit=50&q="
+  URL_ORIGIN = "https://api.spotify.com/v1/search?type=artist&limit=#{ENV["ARTISTS_PER_PAGE" || 50]}&q="
 
   def index
     return :ok
