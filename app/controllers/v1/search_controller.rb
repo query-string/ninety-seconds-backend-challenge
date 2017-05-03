987045fafaeb975bc6672a4c671f42976847b417
Alex Timofeev <atimofeev@reactant.ru>
2017-05-03 13:19:07 +0700

    Make sure that search string has been passed

--- app/controllers/v1/search_controller.rb
+++ app/controllers/v1/search_controller.rb
@@ -2,6 +2,16 @@ class V1::SearchController < ApplicationController
   URL_ORIGIN = "https://api.spotify.com/v1/search?type=artist&limit=#{ENV["ARTISTS_PER_PAGE" || 50]}&q="
 
   def index
-    return :ok
+    if has_query?
+      head :ok
+    else
+      render json: { message: "Search query string `?q=` expected" }, status: :expectation_failed
+    end
+  end
+
+  private
+
+  def has_query?
+    params.include?(:q)
   end
 end
